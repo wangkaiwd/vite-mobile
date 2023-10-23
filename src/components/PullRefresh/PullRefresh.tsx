a078ec1fddb8524743e1c981a842898fc8767d52
wangkaiwd <1162486176@qq.com>
2023-10-23 20:33:05 +0800

    feat: implement pull refresh feature

--- src/components/PullRefresh/PullRefresh.tsx
+++ src/components/PullRefresh/PullRefresh.tsx
@@ -1,30 +1,47 @@
 import './index.less'
 import type { CSSProperties, ReactNode } from 'react'
-import React, { useRef, useState } from 'react'
+import React, { useMemo, useRef, useState } from 'react'
 import { pxToVw } from '../../utils/unit.ts'
 
 interface PullRefreshProps {
   children: ReactNode
 }
 
+const pullDistance = 50
+
+type RefreshStatus = 'normal' | 'loading' | 'success' | 'fail'
+
 const PullRefresh = (props: PullRefreshProps) => {
   const { children } = props
   const startPointRef = useRef<any>(null)
-  const [containerStyle, setContainerStyle] = useState({
-    paddingTop: 0
-  })
-  const [headStyle, setHeadStyle] = useState<CSSProperties>({
+  const headRef = useRef<HTMLDivElement | null>(null)
+  const [deltaY, setDeltaY] = useState(0)
+  const [refreshStatus, setRefreshStatus] = useState<RefreshStatus>('normal')
+  const ease = (distance: number) => {
+    if (distance > pullDistance) {
+      if (distance < pullDistance * 2) {
+        distance = pullDistance + (distance - pullDistance) / 2
+      } else {
+        distance = pullDistance * 1.5 + (distance - pullDistance * 2) / 4
+      }
+    }
+
+    return Math.round(distance)
+  }
+  const containerStyle = useMemo(() => {
+    return {
+      transform: `translateY(${ease(pxToVw(deltaY))}vw)`
+    }
+  }, [deltaY])
+  const headStyle: CSSProperties = {
     transform: 'translateY(-100%)',
-  })
+  }
+
   const onTouchMove = (e: React.TouchEvent) => {
+    if (!headRef.current) {return}
     const { clientY } = e.touches[0]
     const deltaY = clientY - startPointRef.current.clientY
-    setHeadStyle({
-      transform: `translateY(${pxToVw(deltaY)}vw)`,
-    })
-    setContainerStyle({
-      paddingTop: deltaY
-    })
+    setDeltaY(deltaY)
   }
   const onTouchStart = (e: React.TouchEvent) => {
     const { clientX, clientY } = e.touches[0]
@@ -35,12 +52,28 @@ const PullRefresh = (props: PullRefreshProps) => {
   }
   const onTouchEnd = () => {
     startPointRef.current = null
-    setHeadStyle({
-      transform: 'translateY(-100%)',
-    })
-    setContainerStyle({
-      paddingTop: 0
-    })
+    setStatus()
+  }
+  const setStatus = () => {
+    if (deltaY >= pullDistance) { // refresh success
+      setDeltaY(pullDistance)
+      setRefreshStatus('loading')
+      setTimeout(() => {
+        setDeltaY(0)
+        setRefreshStatus('success')
+      }, 1000)
+    }
+  }
+  const renderStatus = () => {
+    if (refreshStatus === 'normal') {
+      return '下拉刷新'
+    } else if (refreshStatus === 'loading') {
+      return '加载中...'
+    } else if (refreshStatus === 'success') {
+      return '加载成功'
+    } else {
+      return '加载失败'
+    }
   }
   return (
     <div
@@ -50,7 +83,9 @@ const PullRefresh = (props: PullRefreshProps) => {
       onTouchMove={onTouchMove}
       onTouchEnd={onTouchEnd}
     >
-      <div className="pull-refresh-head" style={headStyle}>下拉刷新</div>
+      <div ref={headRef} className="pull-refresh-head" style={headStyle}>
+        {renderStatus()}
+      </div>
       {children}
     </div>
   )
